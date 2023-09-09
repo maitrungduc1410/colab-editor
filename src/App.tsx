685bc26259eab31b131c72da225c66eccf5bc90d
Mai Trung Duc <maitrungduc1410@gmail.com>
2023-09-09 16:23:02 +0800

    feat: basic editor setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
-export default function App() {
-  return (
-    <div>Hello world</div>
-  )
+import { useRef, useState } from "react";
+import "./App.css";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import QuillCursors from "quill-cursors";
+import Delta from "quill-delta";
+
+Quill.register("modules/cursors", QuillCursors);
+
+interface User {
+  id: string;
+  color: string;
+  name: string;
 }
+
+function App() {
+  const [value, setValue] = useState<string | Delta>("");
+  const editorRef = useRef<ReactQuill>(null);
+
+  return (
+    <>
+      <ReactQuill
+        ref={editorRef}
+        theme="snow"
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        value={value}
+        modules={{ cursors: true }}
+      />
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PrimeReactProvider } from "primereact/api";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <PrimeReactProvider>
     <App />
-  </React.StrictMode>,
-)
+  </PrimeReactProvider>
+);
